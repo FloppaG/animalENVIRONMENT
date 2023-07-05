ce5d7e5736f08f6d1f78f6c0cdfc19edeedc6230
FloppaG <93928325+FloppaG@users.noreply.github.com>
2023-07-05 18:58:06 +0000

    Fixed everything

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import tkinter as tk
+def gui():
+  import tkinter as tk
 
-win = tk.Tk
-win.mainloop()
+  win = tk.Tk()
+  win.title("GUI")
+
+  apps = tk.Label(win, text="Apps")
+  apps.pack()
+  exit = tk.Button(win, text="Terminal")
+  exit.pack()
+
+  win.mainloop()
+
+gui()
